10fb884ea4f84b9a19ee0e7d504eedcc050c523d
itsme-swe <me.harshmehra07@gmail.com>
2024-10-21 12:51:49 +0530

    update

--- src/OOPs/CircleClass/CircleMainClass.java
+++ src/OOPs/CircleClass/CircleMainClass.java
@@ -6,13 +6,19 @@ public class CircleMainClass {
     // Creating an object of the class "Circle"
     Circle c1 = new Circle();
 
+    Circle c2 = new Circle();
+
     c1.radius = 7;
+
+    c2.radius = 10;
 
     System.out.println("Area: " + c1.area()); // Prints the area of the circle
 
     System.out.println("Perimeter: " + c1.perimeter());
 
     System.out.println("Circumference: " + c1.circumference());
+
+    System.out.println("Area of object c2: " + c2.area());
 
   }
 }
