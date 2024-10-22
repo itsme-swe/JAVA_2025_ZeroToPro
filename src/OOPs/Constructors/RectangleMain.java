8c92aa50996e0cce13f06afa193ee66183c000bf
itsme-swe <me.harshmehra07@gmail.com>
2024-10-23 04:42:46 +0530

    update

--- src/OOPs/Constructors/RectangleMain.java
+++
@@ -1,9 +0,0 @@
-package OOPs.Constructors;
-
-public class RectangleMain {
-  public static void main(String[] args) {
-
-    Rectangle r1 = new Rectangle(10, 5);
-
-  }
-}
--- src/OOPs/ProductClass/ProductMain.java
+++
@@ -1,7 +0,0 @@
-package OOPs.ProductClass;
-
-public class ProductMain {
-  public static void main(String[] args) {
-
-  }
-}
